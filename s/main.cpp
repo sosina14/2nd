97e488e91bb1573e773d4897f08eecff23b04b7f
Sosina Ayele <pittercakity>
2024-09-06 11:24:39 +0300

    this s cpp file folder

---
+++ s/main.cpp
@@ -0,0 +1,42 @@
+/*
+#include <iostream>
+using namespace std;
+
+
+
+
+    void expressFeelings() {
+        cout << "Dear pitter \n\n";
+        cout << "You've captured my heart with the elegance of a well-structured code. ";
+        cout << "Just like a function, you complete me. ";
+        cout << "When I'm with you, I feel like I'm inside a loop of happiness that I never want to break out of.\n\n";
+        cout << "Every moment with you has a lot of meaning ";
+        cout << "and every second with you is like the most optimized algorithm running smoothly.\n\n";
+
+    }
+
+
+int main() {
+//    Love ("YourNameHere");
+    expressFeelings();
+   // infiniteLove(); // This will start the infinite loop
+    return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+
+void Love() {
+        while(true) {
+            cout << "I love you for infinity. 💖\n";
+        }
+    }
+
+int main() {
+   Love();
+return 0;
+}
+
+
+
+
